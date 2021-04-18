0b03ae252fedfef8de7f54f030f6b82a494a9e3d
Miguel Gutierrez <ing.dev.manzano@gmail.com>
2021-04-18 00:15:23 -0500

    cambios addaddress

--- pages/conta.php
+++ pages/conta.php
@@ -15,7 +15,7 @@ if (isset($_POST['submit'])) {
     $mail->Password='#app123456';
 
     $mail->setFrom($_POST['email'],$_POST['nombre']);
-    $mail->addAddress('appcovidcitas@gmail.com');
+    $mail->addAddress($_POST['email'],'appcovidcitas@gmail.com');
     $mail->addReplyTo($_POST['email'],$_POST['nombre']);
 
     $mail->isHTML(true);
